fccc2f3944b71c3d2c2ca5b8b40f9ac4f213d2b4
Livar Cunha <livar@live.com>
2019-11-14 18:44:45 -0800

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.DotNet.Archive;
+
+namespace unarchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var archivePath = args[0];
+            var extractionPath = args[1];
+
+            Directory.CreateDirectory(extractionPath);
+
+            var progress = new ConsoleProgressReport();
+            var archive = new IndexedArchive();
+
+            archive.Extract(archivePath, extractionPath, progress);
+        }
+    }
+}
